c0ee4356b3cad7fa52c7c80a1f36497d0e5cc3b2
liusu_sis@os.ucweb.com <liusu_sis@os.ucweb.com>
2016-06-22 12:05:04 +0800

    module

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,9 +1,20 @@
 package controllers;
 
-import play.mvc.Controller;
+import java.util.HashMap;
+import java.util.Map;
 
+import play.mvc.Controller;
+import play.mvc.With;
+
+@With(Secure.class)
 public class UserController extends Controller {
 
-	
 	// 整合secure模块，实现登录
+
+	public static void get() {
+		// 返回json格式
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", "1");
+		renderJSON(map);
+	}
 }
