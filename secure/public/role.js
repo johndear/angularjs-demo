0b763b54c5eb579a5acc780cbb7d4b1fa6765f1a
johndear <ls123456>
2016-06-28 22:45:29 +0800

    secure

--- secure/public/role.js
+++ secure/public/role.js
@@ -2,6 +2,79 @@ var adminApp = angular.module('adminApp');
 
 adminApp.controller('roleCtrl', ['$scope', '$http', function($scope,$http){
 
+	$scope.abc1 = [ {
+		id : 1,
+		pId : 0,
+		name : "普通的父节点",
+		t : "我很普通，随便点我吧",
+		open : true
+	}, {
+		id : 11,
+		pId : 1,
+		name : "叶子节点 - 1",
+		t : "我很普通，随便点我吧"
+	}, {
+		id : 12,
+		pId : 1,
+		name : "叶子节点 - 2",
+		t : "我很普通，随便点我吧"
+	}, {
+		id : 13,
+		pId : 1,
+		name : "叶子节点 - 3",
+		t : "我很普通，随便点我吧"
+	}, {
+		id : 2,
+		pId : 0,
+		name : "NB的父节点",
+		t : "点我可以，但是不能点我的子节点，有本事点一个你试试看？",
+		open : true
+	}, {
+		id : 21,
+		pId : 2,
+		name : "叶子节点2 - 1",
+		t : "你哪个单位的？敢随便点我？小心点儿..",
+		click : false
+	}, {
+		id : 22,
+		pId : 2,
+		name : "叶子节点2 - 2",
+		t : "我有老爸罩着呢，点击我的小心点儿..",
+		click : false
+	}, {
+		id : 23,
+		pId : 2,
+		name : "叶子节点2 - 3",
+		t : "好歹我也是个领导，别普通群众就来点击我..",
+		click : false
+	}, {
+		id : 3,
+		pId : 0,
+		name : "郁闷的父节点",
+		t : "别点我，我好害怕...我的子节点随便点吧...",
+		open : true,
+		click : false
+	}, {
+		id : 31,
+		pId : 3,
+		name : "叶子节点3 - 1",
+		t : "唉，随便点我吧"
+	}, {
+		id : 32,
+		pId : 3,
+		name : "叶子节点3 - 2",
+		t : "唉，随便点我吧"
+	}, {
+		id : 33,
+		pId : 3,
+		name : "叶子节点3 - 3",
+		t : "唉，随便点我吧"
+	} ];
+	
+	$scope.test = function(){
+		console.log($scope.selectNode);
+	}
+	
 	$scope.bsTableControl = {
             options: {
             	url:'RoleController/query',
@@ -34,7 +107,7 @@ adminApp.controller('roleCtrl', ['$scope', '$http', function($scope,$http){
 			        field: 'opt',
 			        title: '操作',
 			        formatter:function(value, row, index) {
-			        	return '<a>分配资源</a>'
+			        	return '<a onclick="showResource('+ $scope.selectNode +')">分配资源</a>'
 			        }
 			    }],
 			    onClickRow: function(row, $element){
@@ -146,4 +219,6 @@ String.prototype.endWith=function(str){
   var reg=new RegExp(str+"$");     
   return reg.test(this);        
 }
+
+
 
