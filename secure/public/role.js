fd3315a72a7455ff748013a6829683a9d9e406b4
johndear <ls123456>
2016-06-28 08:11:24 +0800

    secure

--- secure/public/role.js
+++ secure/public/role.js
@@ -32,7 +32,10 @@ adminApp.controller('roleCtrl', ['$scope', '$http', function($scope,$http){
 			        title: '备注'
 			    }, {
 			        field: 'opt',
-			        title: '操作'
+			        title: '操作',
+			        formatter:function(value, row, index) {
+			        	return '<a>分配资源</a>'
+			        }
 			    }],
 			    onClickRow: function(row, $element){
 //			    	console.log('row', row);
@@ -90,12 +93,44 @@ adminApp.controller('roleCtrl', ['$scope', '$http', function($scope,$http){
 				    clickToSelect: false,
 				    showToggle: true,
 				    maintainSelected: true,
+//				    detailView: true,
+//				    detailFormatter:function(index, row, element) {
+//				    	return '';
+//				    },
 				    columns: [{
 				        field: 'name',
 				        title: '资源名称'
 				    },{
 				        field: 'actions',
-				        title: '操作名称'
+				        title: '操作名称',
+				        formatter:function(value, row, index) {
+				        	if(value!=undefined){
+				        		if(value.endWith(',')){
+				        			value = value.substring(0, value.length-1);
+				        		}
+				        		var checkBoxList = '';
+				        		// 资源所有操作
+				        		var actions = value.split(',');
+				        		// 资源已授权操作
+				        		if(row.authActions!=undefined){
+				        			var authActions = row.authActions.split(',');
+				        		}
+				        		for(var i in actions){
+				        			var _value = actions[i].split(':')[0];
+				        			var _text = actions[i].split(':')[1];
+				        			// 默认选中已授权操作
+				        			var authed = false;
+				        			for(var j in authActions){
+				        				if(_value == authActions[j]){
+				        					authed = true;
+				        				}
+				        			}
+				        			checkBoxList +=' <input type="checkbox" value="'+ _value +'" '+ (authed? 'checked':'') +'>' + _text;
+				        		}
+				        		return checkBoxList;
+				        	}
+				        	return value;
+					    } 
 				    },{
 				        field: 'authActions',
 				        title: '已授权操作'
@@ -106,4 +141,9 @@ adminApp.controller('roleCtrl', ['$scope', '$http', function($scope,$http){
 	}
 	
 }]);
+
+String.prototype.endWith=function(str){     
+  var reg=new RegExp(str+"$");     
+  return reg.test(this);        
+}
 
