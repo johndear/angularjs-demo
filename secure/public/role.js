625c5007ee8fd4ec4c49a628938e71a302fd6dd4
liusu_sis@os.ucweb.com <liusu_sis@os.ucweb.com>
2016-07-01 12:03:31 +0800

    role

--- secure/public/role.js
+++ secure/public/role.js
@@ -1,80 +1,7 @@
 var adminApp = angular.module('adminApp');
 
-adminApp.controller('roleCtrl', function($scope,$http, $uibModal, $log){
+adminApp.controller('roleCtrl', function($scope,$http, $uibModal, $log, allResources){
 
-	$scope.abc1 = [ {
-		id : 1,
-		pId : 0,
-		name : "普通的父节点",
-		t : "我很普通，随便点我吧",
-		open : true
-	}, {
-		id : 11,
-		pId : 1,
-		name : "叶子节点 - 1",
-		t : "我很普通，随便点我吧"
-	}, {
-		id : 12,
-		pId : 1,
-		name : "叶子节点 - 2",
-		t : "我很普通，随便点我吧"
-	}, {
-		id : 13,
-		pId : 1,
-		name : "叶子节点 - 3",
-		t : "我很普通，随便点我吧"
-	}, {
-		id : 2,
-		pId : 0,
-		name : "NB的父节点",
-		t : "点我可以，但是不能点我的子节点，有本事点一个你试试看？",
-		open : true
-	}, {
-		id : 21,
-		pId : 2,
-		name : "叶子节点2 - 1",
-		t : "你哪个单位的？敢随便点我？小心点儿..",
-		click : false
-	}, {
-		id : 22,
-		pId : 2,
-		name : "叶子节点2 - 2",
-		t : "我有老爸罩着呢，点击我的小心点儿..",
-		click : false
-	}, {
-		id : 23,
-		pId : 2,
-		name : "叶子节点2 - 3",
-		t : "好歹我也是个领导，别普通群众就来点击我..",
-		click : false
-	}, {
-		id : 3,
-		pId : 0,
-		name : "郁闷的父节点",
-		t : "别点我，我好害怕...我的子节点随便点吧...",
-		open : true,
-		click : false
-	}, {
-		id : 31,
-		pId : 3,
-		name : "叶子节点3 - 1",
-		t : "唉，随便点我吧"
-	}, {
-		id : 32,
-		pId : 3,
-		name : "叶子节点3 - 2",
-		t : "唉，随便点我吧"
-	}, {
-		id : 33,
-		pId : 3,
-		name : "叶子节点3 - 3",
-		t : "唉，随便点我吧"
-	} ];
-	
-	$scope.test = function(){
-		console.log($scope.selectNode);
-	}
-	
 	$scope.bsTableControl = {
             options: {
             	url:'RoleController/query',
@@ -107,9 +34,14 @@ adminApp.controller('roleCtrl', function($scope,$http, $uibModal, $log){
 			        field: 'opt',
 			        title: '操作',
 			        formatter:function(value, row, index) {
-			        	return '<a onclick="showResource('+ $scope.selectNode +')">分配资源</a>'
+			        	return '<a onclick="javascript:void(0);">分配资源</a>'
 			        }
 			    }],
+			    onClickCell: function(field, value, row, $element){
+			    	if(field=='opt'){
+			    		$scope.open();
+			    	}
+			    },
 			    onClickRow: function(row, $element){
 //			    	console.log('row', row);
 			    	getResourceAction(row.id);
@@ -214,7 +146,6 @@ adminApp.controller('roleCtrl', function($scope,$http, $uibModal, $log){
 	}
 	
   // 弹出框事件
-  $scope.items = ['item1', 'item2', 'item3'];
   $scope.animationsEnabled = true;
   $scope.open = function (size) {
 	  var modalInstance = $uibModal.open({
@@ -223,8 +154,8 @@ adminApp.controller('roleCtrl', function($scope,$http, $uibModal, $log){
 	      controller: 'ModalInstanceCtrl',
 	      size: size,
 	      resolve: {
-	        items: function () {
-	          return $scope.items;
+	    	resourceList: function () {
+	    		return allResources;
 	        }
 	      }
 	  });
@@ -239,19 +170,20 @@ adminApp.controller('roleCtrl', function($scope,$http, $uibModal, $log){
   $scope.toggleAnimation = function () {
     $scope.animationsEnabled = !$scope.animationsEnabled;
   };
-	
+  
 });
 
 // 弹出框页面controller
-adminApp.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
-
-	  $scope.items = items;
+adminApp.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, resourceList) {
+	
+	  $scope.resources = resourceList;
 	  $scope.selected = {
-	    item: $scope.items[0]
+	    item: $scope.resources[0]
 	  };
 	
 	  $scope.ok = function () {
 		  console.log('ok...');
+		  console.log($scope.selectNode);
 		  $uibModalInstance.close($scope.selected.item);
 	  };
 	
