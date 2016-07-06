ab786127ae4a136f19b28b7d24d0328ea367dcd3
johndear <ls123456>
2016-07-06 08:27:08 +0800

    tree-view

--- secure/public/resource.js
+++ secure/public/resource.js
@@ -1,6 +1,6 @@
 var adminApp = angular.module('adminApp');
 
-adminApp.controller('resourceCtrl', ['$scope', '$http', function($scope,$http){
+adminApp.controller('resourceCtrl', function($scope,$http,$aside){
 
 	$scope.bsTableControl = {
             options: {
@@ -40,52 +40,59 @@ adminApp.controller('resourceCtrl', ['$scope', '$http', function($scope,$http){
             }
 	}
 	
-}]);
+//	// Pre-fetch an external template populated with a custom scope
+//	  var myOtherAside = $aside({scope: $scope, template: 'views/aside.demo.tpl.html'});
+//	  // Show when some event occurs (use $promise property to ensure the template has been loaded)
+//	  myOtherAside.$promise.then(function() {
+//	    myOtherAside.show();
+//	  })
+	
+});
 
 adminApp.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridTreeViewConstants', function ($scope, $http, $interval, uiGridTreeViewConstants ) {
 	
 	$scope.gridOptions = {
-	    enableSorting: true,
+	    enableSorting: false,
 	    enableFiltering: true,
 	    showTreeExpandNoChildren: true,
 	    columnDefs: [
 	      { name: 'name', width: '30%' },
-	      { name: 'gender', width: '20%' },
-	      { name: 'age', width: '20%' },
-	      { name: 'company', width: '25%' },
-	      { name: 'state', width: '35%' },
-	      { name: 'balance', width: '25%', cellFilter: 'currency' }
+	      { name: 'code', width: '20%' },
+	      { name: 'url', width: '20%' },
+	      { name: 'pId', width: '25%' }
+//	      { name: 'state', width: '35%' },
+//	      { name: 'balance', width: '25%', cellFilter: 'currency' }
 	    ],
 	    onRegisterApi: function( gridApi ) {
 	      $scope.gridApi = gridApi;
 	      $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
-	        if( row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded ) {
-	          $interval(function() {
-	            $scope.gridOptions.data.splice(51,0,
-	              {name: 'Dynamic 1', gender: 'female', age: 53, company: 'Griddable grids', balance: 38000, $$treeLevel: 1},
-	              {name: 'Dynamic 2', gender: 'male', age: 18, company: 'Griddable grids', balance: 29000, $$treeLevel: 1}
-	            );
-	            $scope.nodeLoaded = true;
-	          }, 2000, 1);
-	        }
+//	        if( row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded ) {
+//	          $interval(function() {
+//	            $scope.gridOptions.data.splice(51,0,
+//	              {name: 'Dynamic 1', gender: 'female', age: 53, company: 'Griddable grids', balance: 38000, $$treeLevel: 1},
+//	              {name: 'Dynamic 2', gender: 'male', age: 18, company: 'Griddable grids', balance: 29000, $$treeLevel: 1}
+//	            );
+//	            $scope.nodeLoaded = true;
+//	          }, 2000, 1);
+//	        }
 	      });
 	    }
 	  };
 	 
-	  $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+	  $http.get('ResourceController')
 	  .success(function(data) {
-	   for ( var i = 0; i < data.length; i++ ){
-	     data[i].state = data[i].address.state;
-	     data[i].balance = Number( data[i].balance.slice(1).replace(/,/,'') );
-	   }
+//	   for ( var i = 0; i < data.length; i++ ){
+//	     data[i].state = data[i].address.state;
+//	     data[i].balance = Number( data[i].balance.slice(1).replace(/,/,'') );
+//	   }
 	   data[0].$$treeLevel = 0;
-	   data[1].$$treeLevel = 1;
-	   data[10].$$treeLevel = 1;
-	   data[11].$$treeLevel = 1;
-	   data[20].$$treeLevel = 0;
-	   data[25].$$treeLevel = 1;
-	   data[50].$$treeLevel = 0;
-	   data[51].$$treeLevel = 0;
+//	   data[1].$$treeLevel = 1;
+//	   data[10].$$treeLevel = 1;
+//	   data[11].$$treeLevel = 1;
+//	   data[20].$$treeLevel = 0;
+//	   data[25].$$treeLevel = 1;
+//	   data[50].$$treeLevel = 0;
+//	   data[51].$$treeLevel = 0;
 	   $scope.gridOptions.data = data;
 	  });
 	 
